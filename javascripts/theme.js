479add1c01271dbc0628f62784e9dc8375e9f914
ishikawa999 <remember929@gmail.com>
2021-06-18 05:35:47 +0000

    Fix an issue where the absolute date disappears when hovering over a date

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,15 +1,21 @@
+$(document).ajaxSuccess(function() {
+  // Put the title in the before content without breaking the tooltip (For issues/show time_entries and changesets tabs)
+  $('body.controller-issues.action-show a[href*="activity"][title*=":"]:not([data-absolute-date*=":"])').each(function(_index, element){
+    $(element).attr('data-absolute-date', element.title);
+  });
+});
+
 $(function(){
-  /* 外部リンクは新規ウィンドウで開く */
+  /* Change to open external link in another tab */
   $("a.external").attr("target","_blank");
   $("a.help").attr("target","_blank");
-  $("div#footer a[href^='http://www.redmine.org/']").attr("target","_blank");
+  $("div#footer a[href^='https://www.redmine.org/']").attr("target","_blank");
 
-  var lang = $("a.help").text() == "ヘルプ" ? "ja" : $("html").attr("lang");
-  if (lang == "ja") {
-    /* 言語が日本語のときはヘルプのリンク先をRedmine.JPの日本語訳にする */
+  if (($("a.help").text() == "ヘルプ" ? "ja" : $("html").attr("lang")) == "ja") {
+    /* When the language is Japanese, the link destination of help is Redmine.jp */
     $("a.help").attr("href", "http://guide.redmine.jp/");
 
-    /* 検索におけるスコープを検索ボックスのプレースホルダーに表示 */
+    /* Show scope in search in search box placeholder */
     $("body").attr("class").match(/controller-[\S]+/);
     var controllerName = RegExp.lastMatch;
     var placeholderText = "Redmine内を検索"
@@ -23,10 +29,15 @@ $(function(){
     ];
     jQuery.each(placeholderStrings, function() {
       if (controllerName === this.controller) {
-      	placeholderText = this.text;
-      	return false;
+        placeholderText = this.text;
+        return false;
       }
     });
     $("#quick-search input#q").attr("placeholder", placeholderText);
   }
+
+  // Put the title in the before content without breaking the tooltip
+  $('a[href*="activity"][title*=":"]').each(function(_index, element){
+    $(element).attr("data-absolute-date", element.title);
+  });
 });
